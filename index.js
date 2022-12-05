5d796e7e03ea696df7fff151ab6c8219cabcd090
Daniel Youngbar <d.youngbar928@gmail.com>
2022-12-05 10:43:38 -0800

    Separate char variables

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const alphaChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const numChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const symbolChars = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
 let firstPasswordEl = document.getElementById('first-password');
 let secondPasswordEl = document.getElementById('second-password');
+
+let symbolBox = document.getElementById('symbols');
+let numBox = document.getElementById('numbers');
 
 function generate() {
     firstPasswordEl.textContent = newPassword();
